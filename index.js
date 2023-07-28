bfa269aa32b2541d7774cbc4e3ae26bb8c0b9ea9
kailahk <kailahkorsh@me.com>
2023-07-27 19:28:22 -0700

    font change functionality

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const slider1 = document.getElementById('slider-1');
 const slider2 = document.getElementById('slider-2');
 const cake4freaksElement = document.getElementById('cake4freaks-input');
 const henmaniaElement = document.getElementById('henmania-input');
+const dropdown1 = document.getElementById('dropdown-content-1');
+const dropdown2 = document.getElementById('dropdown-content-2');
 
-slider1.addEventListener('input', function() {
+slider1.addEventListener('input', function () {
   cake4freaksElement.style.fontSize = this.value + 'px';
 });
 
-slider2.addEventListener('input', function() {
+slider2.addEventListener('input', function () {
   henmaniaElement.style.fontSize = this.value + 'px';
-})
+});
+
+dropdown1.addEventListener('click', function (evt) {
+  if (evt.target.innerHTML === "CAKE4FREAKS") {
+    cake4freaksElement.style.fontFamily = "Cake4Freaks-Regular";
+  } else {
+    cake4freaksElement.style.fontFamily = "Henmania-Black";
+  }
+});
+
+dropdown2.addEventListener('click', function (evt) {
+  if (evt.target.innerHTML === "CAKE4FREAKS") {
+    henmaniaElement.style.fontFamily = "Cake4Freaks-Regular";
+  } else {
+    henmaniaElement.style.fontFamily = "Henmania-Black";
+  }
+});
